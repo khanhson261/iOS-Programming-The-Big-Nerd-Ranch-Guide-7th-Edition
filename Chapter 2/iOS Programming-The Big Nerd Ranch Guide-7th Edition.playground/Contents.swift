e5264b51f2e97b24802efb49321c23dd8c20636b
Son Tran <61035327+khanhson261@users.noreply.github.com>
2023-06-11 15:06:54 +0700

    add chapter 2 content

---
+++ Chapter 2/iOS Programming-The Big Nerd Ranch Guide-7th Edition.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+var nextYear: Int = 0
+var bodyTemp: Float = 0
+var hasPet: Bool = true
+let defaultNumber = Int() 
+let defaultBool = Bool()
