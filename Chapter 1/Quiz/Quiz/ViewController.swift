d3e272cb480fefd0022261d72670b30c5f43cbb4
Son Tran <61035327+khanhson261@users.noreply.github.com>
2023-06-11 14:38:25 +0700

    add Chapter 1 content

---
+++ Chapter 1/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Khanh Son on 6/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    var currentQuestionIndex: Int = 0
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes" ]
+}
+
